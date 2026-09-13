2baea25ae674f01d17f89dc226b4ef14747170e0
chalisa1103 <chalisa.nk@mail.wu.ac.th>
2026-09-13 21:07:45 +0700

    Initial commit
    A

--- components/StockTable.tsx
+++ components/StockTable.tsx
@@ -206,7 +206,7 @@ export default function StockTable({
   const renderPaginationButtons = () => {
     const buttons = [];
     let startPage = Math.max(1, currentPage - 1);
-    let endPage = Math.min(totalPages, startPage + 2);
+    const endPage = Math.min(totalPages, startPage + 2);
 
     if (endPage - startPage < 2) {
       startPage = Math.max(1, endPage - 2);
